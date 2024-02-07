30e906c712ef746b360bbed71140d2f9623541ae
U-IOI-U <74693141+U-IOI-U@users.noreply.github.com>
2024-02-07 14:45:55 +0800

    add snakem982/proxypool

--- scripts/build_proxypool_source.sh
+++ scripts/build_proxypool_source.sh
@@ -68,10 +68,7 @@ git_clone_repo "https://github.com/lee-alone/proxypool_source.yaml.git" "main" "
 if [ -d "lee-alone" ]; then
     if [ -f "lee-alone/source.yaml" ]; then
         {
-            echo "- type: clash"
-            echo "  options:"
-            echo "    url: https://github.com/lee-alone/proxypool_source.yaml/raw/main/source.yaml"
-            echo ""
+            cat "lee-alone/source.yaml"
         } >> /workdir/pp_source.yaml
     fi
     if [ -f "lee-alone/diylist" ]; then
@@ -83,4 +80,180 @@ if [ -d "lee-alone" ]; then
         } >> /workdir/pp_source.yaml
     fi
     rm -rf "lee-alone"
+fi
+
+echo "[ snakem982/proxypool ]"
+git_clone_repo "https://github.com/snakem982/proxypool.git" "main" "proxypool"
+if [ -d "proxypool" ]; then
+    if [ -f "proxypool/nodelist.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/snakem982/proxypool/raw/main/nodelist.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/proxies.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/snakem982/proxypool/raw/main/proxies.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/webfuzz.yaml" ]; then
+        {
+            cat "proxypool/webfuzz.yaml"
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/tgchannel.json" ]; then
+        {
+            for i in `grep '"' proxypool/tgchannel.json | awk -F '"' '{print $2}'`; do
+                echo "- type: tgchannel"
+                echo "  options:"
+                echo "    channel: $i"
+                echo "    num: 100"
+                echo ""
+            done
+        } >> /workdir/pp_source.yaml
+    fi
+    rm -rf "proxypool"
+fi
+
+echo "[ zyjia/proxypool ]"
+git_clone_repo "https://github.com/zyjia/proxypool.git" "main" "proxypool"
+if [ -d "proxypool" ]; then
+    if [ -f "proxypool/nodelist.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/zyjia/proxypool/raw/main/nodelist.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/proxies.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/zyjia/proxypool/raw/main/proxies.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/webfuzz.yaml" ]; then
+        {
+            cat "proxypool/webfuzz.yaml"
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/tgchannel.json" ]; then
+        {
+            for i in `grep '"' proxypool/tgchannel.json | awk -F '"' '{print $2}'`; do
+                echo "- type: tgchannel"
+                echo "  options:"
+                echo "    channel: $i"
+                echo "    num: 100"
+                echo ""
+            done
+        } >> /workdir/pp_source.yaml
+    fi
+    rm -rf "proxypool"
+fi
+
+echo "[ xiaoqi33221/xiaoqi-Node-pools ]"
+git_clone_repo "https://github.com/xiaoqi33221/xiaoqi-Node-Pools.git" "main" "proxypool"
+if [ -d "proxypool" ]; then
+    if [ -f "proxypool/nodelist.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/xiaoqi33221/xiaoqi-Node-Pools/raw/main/nodelist.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/proxies.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/xiaoqi33221/xiaoqi-Node-Pools/raw/main/proxies.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/webfuzz.yaml" ]; then
+        {
+            cat "proxypool/webfuzz.yaml"
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/tgchannel.json" ]; then
+        {
+            for i in `grep '"' proxypool/tgchannel.json | awk -F '"' '{print $2}'`; do
+                echo "- type: tgchannel"
+                echo "  options:"
+                echo "    channel: $i"
+                echo "    num: 100"
+                echo ""
+            done
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/clash058fb.yaml" ]; then
+        {
+            echo "- type: clash"
+            echo "  options:"
+            echo "    url: https://github.com/xiaoqi33221/xiaoqi-Node-pools/raw/main/clash058fb.yaml"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    rm -rf "proxypool"
+fi
+
+echo "[ hjchjchjc4352/proxypool ]"
+git_clone_repo "https://github.com/hjchjchjc4352/proxypool.git" "main" "proxypool"
+if [ -d "proxypool" ]; then
+    if [ -f "proxypool/nodelist.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/hjchjchjc4352/proxypool/raw/main/nodelist.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/proxies.txt" ]; then
+        {
+            echo "- type: webfuzzsub"
+            echo "  options:"
+            echo "    url: https://github.com/hjchjchjc4352/proxypool/raw/main/proxies.txt"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/webfuzz.yaml" ]; then
+        {
+            cat "proxypool/webfuzz.yaml"
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/tgchannel.json" ]; then
+        {
+            for i in `grep '"' proxypool/tgchannel.json | awk -F '"' '{print $2}'`; do
+                echo "- type: tgchannel"
+                echo "  options:"
+                echo "    channel: $i"
+                echo "    num: 100"
+                echo ""
+            done
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/clash.yaml" ]; then
+        {
+            echo "- type: clash"
+            echo "  options:"
+            echo "    url: https://github.com/hjchjchjc4352/proxypool/raw/main/clash.yaml"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    if [ -f "proxypool/mihomo.yaml" ]; then
+        {
+            echo "- type: clash"
+            echo "  options:"
+            echo "    url: https://github.com/hjchjchjc4352/proxypool/raw/main/mihomo.yaml"
+            echo ""
+        } >> /workdir/pp_source.yaml
+    fi
+    rm -rf "proxypool"
 fi
